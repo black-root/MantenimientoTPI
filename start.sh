ff2f7a10e719260321d1a3eeb40a6b088b4a6af1
Ronald Ibanez <ronald_reir21@hotmail.com>
2018-03-26 02:43:15 -0600

    se arreglo problema sobre el despliege dockerfiles and scripts

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+docker rm conpayaratpi
+docker rm conmdbtpi
+
 docker rmi -f payaratpi
 docker rmi -f mariadbtpi:IR13002
 
 mvn clean install
-
-mv /MantenimientoTPI-ear/target/MantenimientoTPI-ear-1.0-SNAPSHOT.ear /Payarafull/MantenimientoTPI-ear-1.0-SNAPSHOT.ear
+rm Payarafull/MantenimientoTPI-ear-1.0-SNAPSHOT.ear
+mv MantenimientoTPI-ear/target/MantenimientoTPI-ear-1.0-SNAPSHOT.ear Payarafull/
 
 docker build -t mariadbtpi:IR13002 ./MariaDB
 docker build -t payaratpi ./Payarafull
