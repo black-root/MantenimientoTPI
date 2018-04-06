5c61466ec8d2495254a0745f49393567a5ff3886
Ronald Ibanez <ronald_reir21@hotmail.com>
2018-04-06 14:26:17 -0600

    funciona

--- start.sh
+++ start.sh
@@ -2,18 +2,17 @@
 docker rm conpayaratpi
 docker rm conmdbtpi
 
-docker rmi -f payaratpi
-docker rmi -f mariadbtpi
+#docker rmi -f payaratpi
+#docker rmi -f mariadbtpi
 
 mvn clean install
 #rm Payarafull/MantenimientoTPI-ear-1.0-SNAPSHOT.ear
 #mv MantenimientoTPI-ear/target/MantenimientoTPI-ear-1.0-SNAPSHOT.ear Payarafull/
-rm  Payarafull/MantenimientoTPI-web.war
-mv  MantenimientoTPI-web/target/MantenimientoTPI-web-1.0-SNAPSHOT.war Payarafull/MantenimientoTPI-web.war
+rm  Payaramicro/MantenimientoTPI-web.war
+mv  MantenimientoTPI-web/target/MantenimientoTPI-web-1.0-SNAPSHOT.war PayaraMicro/MantenimientoTPI-web.war
 
-
-docker build -t mariadbtpi ./MariaDB
-docker build -t payaratpi ./Payarafull
+#docker build -t mariadbtpi ./MariaDB
+#docker build -t payaratpi ./Payarafull
 
 docker-compose up
 
