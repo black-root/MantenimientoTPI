5c8d9b719a5ffceb717e41bb39ca18ac2d71fedd
Ronald Ibanez <ronald_reir21@hotmail.com>
2018-04-06 14:36:29 -0600

    se dejo mas limpio el script

--- start.sh
+++ start.sh
@@ -2,17 +2,11 @@
 docker rm conpayaratpi
 docker rm conmdbtpi
 
-#docker rmi -f payaratpi
-#docker rmi -f mariadbtpi
-
 mvn clean install
-#rm Payarafull/MantenimientoTPI-ear-1.0-SNAPSHOT.ear
-#mv MantenimientoTPI-ear/target/MantenimientoTPI-ear-1.0-SNAPSHOT.ear Payarafull/
+
 rm  Payaramicro/MantenimientoTPI-web.war
 mv  MantenimientoTPI-web/target/MantenimientoTPI-web-1.0-SNAPSHOT.war PayaraMicro/MantenimientoTPI-web.war
 
-#docker build -t mariadbtpi ./MariaDB
-#docker build -t payaratpi ./Payarafull
-
+#google-chrome  http://localhost:8080/MantenimientoTPI-web/
 docker-compose up
 
