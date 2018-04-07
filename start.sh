b863c0d3ad11a0b6cb954b285ada644277af80f7
Ronald Ibañez <ronald_reir21@hotmail.com>
2018-04-06 23:18:01 -0600

    Update start.sh
    fix de nombre de carpeta Payaramicro

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ docker rm conmdbtpi
 mvn clean install
 
 rm  Payaramicro/MantenimientoTPI-web.war
-mv  MantenimientoTPI-web/target/MantenimientoTPI-web-1.0-SNAPSHOT.war PayaraMicro/MantenimientoTPI-web.war
+mv  MantenimientoTPI-web/target/MantenimientoTPI-web-1.0-SNAPSHOT.war Payaramicro/MantenimientoTPI-web.war
 
 #google-chrome  http://localhost:8080/MantenimientoTPI-web/
 docker-compose up
