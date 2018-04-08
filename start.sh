0e96e8ea7a1dbc97dd4f5ef47ddf3e2beaab443b
Ronald Ibanez <ronald_reir21@hotmail.com>
2018-04-07 19:41:37 -0600

    Se arreglo script, ya actuliza

--- start.sh
+++ start.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-docker rm conpayaratpi
-docker rm conmdbtpi
+#docker rm conpayaratpi
+#docker rm conmdbtpi
+
+
+docker rm mariamicro
+docker rm paymicro
+
+docker rmi -f mariadbtpi
+docker rmi -f sergiopa
+
+docker build -t mariadbtpi ./MariaDB
+docker build -t sergiopa ./Payaramicro
+
 
 mvn clean install
 
 rm  Payaramicro/MantenimientoTPI-web.war
-mv  MantenimientoTPI-web/target/MantenimientoTPI-web-1.0-SNAPSHOT.war Payaramicro/MantenimientoTPI-web.war
+cp  MantenimientoTPI-web/target/MantenimientoTPI-web-1.0-SNAPSHOT.war Payaramicro/MantenimientoTPI-web.war
 
 #google-chrome  http://localhost:8080/MantenimientoTPI-web/
 docker-compose up
