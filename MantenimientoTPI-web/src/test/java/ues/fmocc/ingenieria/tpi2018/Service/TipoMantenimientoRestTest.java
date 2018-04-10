aff88dfe6ebcc607f614cc35a7377a4ce1ee8613
viktor <viktor@DebVk.lan>
2018-04-10 10:15:49 -0600

    Test completos para TipoMantenimientoRest

--- MantenimientoTPI-web/src/test/java/ues/fmocc/ingenieria/tpi2018/Service/TipoMantenimientoRestTest.java
+++ MantenimientoTPI-web/src/test/java/ues/fmocc/ingenieria/tpi2018/Service/TipoMantenimientoRestTest.java
@@ -5,13 +5,19 @@
  */
 package ues.fmocc.ingenieria.tpi2018.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.times;
+import org.powermock.api.mockito.PowerMockito;
 import ues.fmoocc.ingenieria.tpi2018.Entities.TipoMantenimiento;
 
 /**
@@ -19,6 +25,8 @@ import ues.fmoocc.ingenieria.tpi2018.Entities.TipoMantenimiento;
  * @author viktor
  */
 public class TipoMantenimientoRestTest {
+    final TipoMantenimientoRest mokTMRest = Mockito.mock(TipoMantenimientoRest.class);
+    final TipoMantenimiento mokTM = Mockito.mock(TipoMantenimiento.class);
     
     public TipoMantenimientoRestTest() {
     }
@@ -32,7 +40,17 @@ public class TipoMantenimientoRestTest {
     }
     
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        PowerMockito.whenNew(TipoMantenimiento.class).withAnyArguments().thenReturn(mokTM);
+        Mockito.when(mokTMRest.count()).thenReturn(1);
+        TipoMantenimiento tm = new TipoMantenimiento(1);
+        TipoMantenimiento tm1 = new TipoMantenimiento(2);
+        List<TipoMantenimiento> listaTM = new ArrayList<>();
+        listaTM.add(tm);
+        listaTM.add(tm1);
+        Mockito.when(mokTMRest.findall()).thenReturn(listaTM);
+        Mockito.when(mokTMRest.findById(1)).thenReturn(tm);
+        Mockito.when(mokTMRest.findByDescripcion("descripcion")).thenReturn(listaTM);
     }
     
     @After
@@ -45,12 +63,11 @@ public class TipoMantenimientoRestTest {
     @Test
     public void testFindall() {
         System.out.println("findall");
-        TipoMantenimientoRest instance = new TipoMantenimientoRest();
-        List<TipoMantenimiento> expResult = null;
-        List<TipoMantenimiento> result = instance.findall();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        TipoMantenimientoRest rest = mokTMRest;
+        List<TipoMantenimiento> result = rest.findall();
+        TipoMantenimiento tm = new TipoMantenimiento(1);
+        assertThat(result, CoreMatchers.hasItems(tm));
+                
     }
 
     /**
@@ -59,12 +76,10 @@ public class TipoMantenimientoRestTest {
     @Test
     public void testCount() {
         System.out.println("count");
-        TipoMantenimientoRest instance = new TipoMantenimientoRest();
-        Integer expResult = null;
-        Integer result = instance.count();
+        TipoMantenimientoRest instance = mokTMRest;
+        int expResult = 1;
+        int result = instance.count();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -73,13 +88,11 @@ public class TipoMantenimientoRestTest {
     @Test
     public void testFindById() {
         System.out.println("findById");
-        int id = 0;
-        TipoMantenimientoRest instance = new TipoMantenimientoRest();
-        TipoMantenimiento expResult = null;
+        int id = 1;
+        TipoMantenimientoRest instance = mokTMRest;
+        TipoMantenimiento expResult = new TipoMantenimiento(1);
         TipoMantenimiento result = instance.findById(id);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -88,13 +101,11 @@ public class TipoMantenimientoRestTest {
     @Test
     public void testFindByDescripcion() {
         System.out.println("findByDescripcion");
-        String descripcion = "";
-        TipoMantenimientoRest instance = new TipoMantenimientoRest();
-        List<TipoMantenimiento> expResult = null;
-        List<TipoMantenimiento> result = instance.findByDescripcion(descripcion);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        TipoMantenimientoRest instance = mokTMRest;
+        TipoMantenimiento expResult = new TipoMantenimiento(1);
+        List<TipoMantenimiento> result = instance.findByDescripcion("descripcion");
+        assertThat(result, CoreMatchers.hasItem(expResult));
+        assertNotNull(result);
     }
 
     /**
@@ -102,12 +113,11 @@ public class TipoMantenimientoRestTest {
      */
     @Test
     public void testRemove() {
-        System.out.println("remove");
-        Integer id = null;
-        TipoMantenimientoRest instance = new TipoMantenimientoRest();
-        instance.remove(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Mockito.doNothing().when(mokTMRest).remove((Integer) Matchers.anyObject());
+        mokTMRest.remove(Integer.SIZE);
+        Mockito.verify(mokTMRest, times(1)).remove(Integer.SIZE);
+        Mockito.doThrow(Exception.class).when(mokTMRest).remove(Integer.SIZE);
+   
     }
 
     /**
@@ -116,11 +126,11 @@ public class TipoMantenimientoRestTest {
     @Test
     public void testCreate() {
         System.out.println("create");
-        TipoMantenimiento entity = null;
-        TipoMantenimientoRest instance = new TipoMantenimientoRest();
-        instance.create(entity);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Mockito.doNothing().when(mokTMRest).create(mokTM);
+        mokTMRest.create(mokTM);
+        Mockito.verify(mokTMRest, times(1)).create(mokTM);
+        Mockito.doThrow(Exception.class).when(mokTMRest).create(mokTM);
+  
     }
 
     /**
@@ -129,12 +139,12 @@ public class TipoMantenimientoRestTest {
     @Test
     public void testEdit() {
         System.out.println("edit");
-        Integer id = null;
-        TipoMantenimiento entity = null;
-        TipoMantenimientoRest instance = new TipoMantenimientoRest();
-        instance.edit(id, entity);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Mockito.doNothing().when(mokTMRest).edit(Integer.SIZE, mokTM);
+        mokTMRest.edit(Integer.SIZE, mokTM);
+        Mockito.verify(mokTMRest, times(1)).edit(Integer.SIZE, mokTM);
+        Mockito.doThrow(Exception.class).when(mokTMRest).edit(Integer.SIZE, mokTM);
+  
+        
     }
     
 }
