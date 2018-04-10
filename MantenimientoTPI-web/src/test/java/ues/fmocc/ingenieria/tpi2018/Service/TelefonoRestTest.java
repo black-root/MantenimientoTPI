72c32cf298b7c8fb4588d17d94b2222bc45a9e60
viktor <viktor@DebVk.lan>
2018-04-10 10:00:03 -0600

    Test completos para TelefonoRest

--- MantenimientoTPI-web/src/test/java/ues/fmocc/ingenieria/tpi2018/Service/TelefonoRestTest.java
+++ MantenimientoTPI-web/src/test/java/ues/fmocc/ingenieria/tpi2018/Service/TelefonoRestTest.java
@@ -5,13 +5,19 @@
  */
 package ues.fmocc.ingenieria.tpi2018.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.times;
+import org.powermock.api.mockito.PowerMockito;
 import ues.fmoocc.ingenieria.tpi2018.Entities.Telefono;
 
 /**
@@ -19,6 +25,8 @@ import ues.fmoocc.ingenieria.tpi2018.Entities.Telefono;
  * @author viktor
  */
 public class TelefonoRestTest {
+    final TelefonoRest mokTelefonoRest = Mockito.mock(TelefonoRest.class);
+    final Telefono mokTelefono = Mockito.mock(Telefono.class);
     
     public TelefonoRestTest() {
     }
@@ -32,7 +40,16 @@ public class TelefonoRestTest {
     }
     
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        PowerMockito.whenNew(Telefono.class).withAnyArguments().thenReturn(mokTelefono);
+        Mockito.when(mokTelefonoRest.count()).thenReturn(1);
+        Telefono t = new Telefono(1);
+        Telefono t1 = new Telefono(2);
+        List<Telefono> listaTel = new ArrayList<>();
+        listaTel.add(t);
+        listaTel.add(t1);
+        Mockito.when(mokTelefonoRest.findall()).thenReturn(listaTel);
+        Mockito.when(mokTelefonoRest.findById(1)).thenReturn(t);
     }
     
     @After
@@ -45,12 +62,10 @@ public class TelefonoRestTest {
     @Test
     public void testFindall() {
         System.out.println("findall");
-        TelefonoRest instance = new TelefonoRest();
-        List<Telefono> expResult = null;
+        TelefonoRest instance = mokTelefonoRest;
+        Telefono tel = new Telefono(1);
         List<Telefono> result = instance.findall();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertThat(result, CoreMatchers.hasItems(tel));
     }
 
     /**
@@ -59,12 +74,11 @@ public class TelefonoRestTest {
     @Test
     public void testCount() {
         System.out.println("count");
-        TelefonoRest instance = new TelefonoRest();
-        Integer expResult = null;
-        Integer result = instance.count();
+        TelefonoRest instance = mokTelefonoRest;
+        int expResult = 1;
+        int result = instance.count();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
@@ -73,13 +87,11 @@ public class TelefonoRestTest {
     @Test
     public void testFindById() {
         System.out.println("findById");
-        Integer id = null;
-        TelefonoRest instance = new TelefonoRest();
-        Telefono expResult = null;
+        int id=1;
+        TelefonoRest instance = mokTelefonoRest;
+        Telefono expResult = new Telefono(1);
         Telefono result = instance.findById(id);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -87,12 +99,11 @@ public class TelefonoRestTest {
      */
     @Test
     public void testRemove() {
-        System.out.println("remove");
-        Integer id = null;
-        TelefonoRest instance = new TelefonoRest();
-        instance.remove(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Mockito.doNothing().when(mokTelefonoRest).remove((Integer) Matchers.anyObject());
+        mokTelefonoRest.remove(Integer.SIZE);
+        Mockito.verify(mokTelefonoRest, times(1)).remove(Integer.SIZE);
+        Mockito.doThrow(Exception.class).when(mokTelefonoRest).remove(Integer.SIZE);
+   
     }
 
     /**
@@ -101,11 +112,10 @@ public class TelefonoRestTest {
     @Test
     public void testCreate() {
         System.out.println("create");
-        Telefono entity = null;
-        TelefonoRest instance = new TelefonoRest();
-        instance.create(entity);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+                Mockito.doNothing().when(mokTelefonoRest).create(mokTelefono);
+        mokTelefonoRest.create(mokTelefono);
+        Mockito.verify(mokTelefonoRest, times(1)).create(mokTelefono);
+        Mockito.doThrow(Exception.class).when(mokTelefonoRest).create(mokTelefono);
     }
 
     /**
@@ -113,13 +123,11 @@ public class TelefonoRestTest {
      */
     @Test
     public void testEdit() {
-        System.out.println("edit");
-        Integer id = null;
-        Telefono entity = null;
-        TelefonoRest instance = new TelefonoRest();
-        instance.edit(id, entity);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        Mockito.doNothing().when(mokTelefonoRest).edit(Integer.SIZE, mokTelefono);
+        mokTelefonoRest.edit(Integer.SIZE, mokTelefono);
+        Mockito.verify(mokTelefonoRest, times(1)).edit(Integer.SIZE, mokTelefono);
+        Mockito.doThrow(Exception.class).when(mokTelefonoRest).edit(Integer.SIZE, mokTelefono);
     }
     
 }
