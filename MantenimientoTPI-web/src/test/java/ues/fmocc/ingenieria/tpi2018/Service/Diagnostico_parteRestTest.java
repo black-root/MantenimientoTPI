a34af793047052bcda7db1dacc13ac6fdf908f4c
viktor <viktor@DebVk.lan>
2018-04-09 23:09:05 -0600

    Test completos para Diagnostico_parte

--- MantenimientoTPI-web/src/test/java/ues/fmocc/ingenieria/tpi2018/Service/Diagnostico_parteRestTest.java
+++ MantenimientoTPI-web/src/test/java/ues/fmocc/ingenieria/tpi2018/Service/Diagnostico_parteRestTest.java
@@ -5,15 +5,20 @@
  */
 package ues.fmocc.ingenieria.tpi2018.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.embeddable.EJBContainer;
 import javax.ws.rs.core.Response;
+import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.times;
+import org.powermock.api.mockito.PowerMockito;
 import ues.fmoocc.ingenieria.tpi2018.Entities.Dianosticoparte;
 
 /**
@@ -21,7 +26,8 @@ import ues.fmoocc.ingenieria.tpi2018.Entities.Dianosticoparte;
  * @author viktor
  */
 public class Diagnostico_parteRestTest {
-    
+    final Diagnostico_parteRest mokDiagnostico_parteRest = Mockito.mock(Diagnostico_parteRest.class);
+    final Dianosticoparte mokDianosticoparte = Mockito.mock(Dianosticoparte.class); 
     public Diagnostico_parteRestTest() {
     }
     
@@ -34,7 +40,16 @@ public class Diagnostico_parteRestTest {
     }
     
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        PowerMockito.whenNew(Dianosticoparte.class).withAnyArguments().thenReturn(mokDianosticoparte);
+        Dianosticoparte d = new Dianosticoparte(1, "descripcion");
+        Dianosticoparte d2 = new Dianosticoparte(2, "descripcion");
+        List<Dianosticoparte> listadianosticoparte = new ArrayList<>();
+        listadianosticoparte.add(d);
+        listadianosticoparte.add(d2);
+        Mockito.when(mokDiagnostico_parteRest.findAll()).thenReturn(listadianosticoparte);
+        Mockito.when(mokDiagnostico_parteRest.findById(1)).thenReturn(d);
+        
     }
     
     @After
@@ -47,14 +62,11 @@ public class Diagnostico_parteRestTest {
     @Test
     public void testFindAll() throws Exception {
         System.out.println("findAll");
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        Diagnostico_parteRest instance = (Diagnostico_parteRest)container.getContext().lookup("java:global/classes/Diagnostico_parteRest");
-        List<Dianosticoparte> expResult = null;
-        List<Dianosticoparte> result = instance.findAll();
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Diagnostico_parteRest rest = mokDiagnostico_parteRest;
+        List<Dianosticoparte> result = rest.findAll();
+        Dianosticoparte diagParte = new Dianosticoparte(1, "descripcion");
+        assertThat(result, CoreMatchers.hasItem(diagParte));
+        assertNotNull(rest);
     }
 
     /**
@@ -63,15 +75,12 @@ public class Diagnostico_parteRestTest {
     @Test
     public void testFindById() throws Exception {
         System.out.println("findById");
-        int id = 0;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        Diagnostico_parteRest instance = (Diagnostico_parteRest)container.getContext().lookup("java:global/classes/Diagnostico_parteRest");
-        Dianosticoparte expResult = null;
+        int id = 1;
+        Diagnostico_parteRest instance = mokDiagnostico_parteRest;
+        Dianosticoparte expResult = new Dianosticoparte(1, "descripcion");
         Dianosticoparte result = instance.findById(id);
+        //assertNotNull(result);
         assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -81,14 +90,12 @@ public class Diagnostico_parteRestTest {
     public void testBorrarDiagnosticoparte() throws Exception {
         System.out.println("borrarDiagnosticoparte");
         Integer id = null;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        Diagnostico_parteRest instance = (Diagnostico_parteRest)container.getContext().lookup("java:global/classes/Diagnostico_parteRest");
-        Response expResult = null;
+        Diagnostico_parteRest instance = mokDiagnostico_parteRest;
+        Dianosticoparte expResult = null;
         Response result = instance.borrarDiagnosticoparte(id);
         assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNull(result);
+        Mockito.verify(mokDiagnostico_parteRest,times(1)).borrarDiagnosticoparte(id);
     }
 
     /**
@@ -97,15 +104,14 @@ public class Diagnostico_parteRestTest {
     @Test
     public void testGuardarDiagnosticoparte() throws Exception {
         System.out.println("guardarDiagnosticoparte");
-        Dianosticoparte dp = null;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        Diagnostico_parteRest instance = (Diagnostico_parteRest)container.getContext().lookup("java:global/classes/Diagnostico_parteRest");
-        Response expResult = null;
-        Response result = instance.guardarDiagnosticoparte(dp);
+        int id = 1;
+        Diagnostico_parteRest instance = mokDiagnostico_parteRest;
+        Dianosticoparte expResult = null;
+        Response result= instance.guardarDiagnosticoparte(mokDianosticoparte);
         assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNull(result);
+        Mockito.verify(mokDiagnostico_parteRest,times(1)).guardarDiagnosticoparte(mokDianosticoparte);
+            
     }
 
     /**
@@ -113,17 +119,14 @@ public class Diagnostico_parteRestTest {
      */
     @Test
     public void testEditarDiagnosticoparte() throws Exception {
-        System.out.println("editarDiagnosticoparte");
-        Integer id = null;
-        Dianosticoparte dp = null;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        Diagnostico_parteRest instance = (Diagnostico_parteRest)container.getContext().lookup("java:global/classes/Diagnostico_parteRest");
-        Response expResult = null;
-        Response result = instance.editarDiagnosticoparte(id, dp);
+     int id=1;
+        Diagnostico_parteRest instance = mokDiagnostico_parteRest;
+        Dianosticoparte expResult = null;
+        Response result= instance.editarDiagnosticoparte(id, expResult);
         assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNull(result);
+        Mockito.verify(mokDiagnostico_parteRest,times(1)).editarDiagnosticoparte(id, expResult);
+   
     }
     
 }
